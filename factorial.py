1b657a309c1db4cfafb1db78f3ea1ea334e86566
SHARANYA <sharanyaaa768@gmail.com>
2026-02-21 23:53:10 +0530

    save file

--- factorial.py
+++ factorial.py
@@ -3,4 +3,5 @@ product = 1
 for i in range(1,n+1):
     product = product * i
 
-    print(f"the factorial of {n} is {product}")
+
+    print(f"the factorial of {n} is {product}") 
--- pythonnn2.py
+++ pythonnn2.py
@@ -1,6 +1,6 @@
-a=56
-b=7
+a=56 
+
+b=2
+
 print("remainder when a is divided by b is",a % b)
-
-
 
